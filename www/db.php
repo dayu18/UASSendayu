57974edecbb8c1f3e3c1ad656bac0cba02f2336f
dayu18 <38527583+dayu18@users.noreply.github.com>
2018-07-09 15:17:37 +0700

    Add files via upload

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+//  header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id6374924_sendayu","Unisbank01*","id6374924_pembelian") or die ("could not connect database");
+?>
